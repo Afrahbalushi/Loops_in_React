0d942d773176aba2f25768dfbf9e5043f4739778
Afrah Ali <116621717+Afrahbalushi@users.noreply.github.com>
2023-12-27 19:23:50 +0400

    React Virtualized - Grid

--- src/App.js
+++ src/App.js
@@ -1,56 +1,56 @@
-import { List } from 'react-virtualized';
-import './App.css';
+import React from 'react';
+import { List, Grid } from 'react-virtualized';
+import 'react-virtualized/styles.css'; // Import the styles for react-virtualized
 
 function App() {
-  const numbers = [1,2,3,4,5,6,7,8,9,10]
-  let sum = 0
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let sum = 0;
 
-  //sum
-  for(let i =0; i < numbers.length; i++) {
-    
-    sum += numbers[i]
-
+  // sum
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
   }
-  
-  //average
+
+  // average
   const average = numbers.length === 0 ? 0 : sum / numbers.length;
 
-  //fillter even numbers
-  const evenNumbers = []
-  for(let i = 0; i < numbers.length; i++){
-     if(numbers[i] % 2 === 0){
-        evenNumbers.push(numbers[i]);
-     }
+  // filter even numbers
+  const evenNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      evenNumbers.push(numbers[i]);
+    }
   }
 
-//React virtualized
+  // React virtualized
+  const data = ['Apple', 'Orange', 'Banana', 'Strawberry'];
 
-  const data = ['item 1', 'item 2', 'item 3', 'item 4']
+  const rowRender = ({ index, key, style }) => (
+    <div key={key} style={style}>
+      {data[index]}
+    </div>
+  );
 
-  const rowRender = ({index, key, style}) => (
-     <div key={key} style={style}>
-       {data[index]}
-     </div>
-  )
+  const cellRenderer = ({ columnIndex, key, rowIndex, style }) => (
+    <div key={key} style={style}>
+      {data[rowIndex * 2 + columnIndex]}
+    </div>
+  );
 
   return (
     <div className="App">
-     <h1>JavaScript Looping</h1>
-     <h4>List of Numbers = [ {numbers.join(' , ')} ]</h4>
+      <h1>JavaScript Looping</h1>
+      <h4>List of Numbers = [ {numbers.join(' , ')} ]</h4>
       <h3>SUM : {sum}</h3>
       <h3>AVERAGE : {average}</h3>
-      <h3>FILLTERING : {evenNumbers.join(' , ')}</h3>
+      <h3>FILTERING : {evenNumbers.join(' , ')}</h3>
 
       <hr></hr>
 
       <h1>React Virtualized</h1>
-      <List 
-      width={300}
-      height={400}
-      rowCount={data.length}
-      rowHeight={40}
-      rowRenderer={rowRender}
-      />
+      <List width={300} height={400} rowCount={data.length} rowHeight={40} rowRenderer={rowRender} />
+
+      <Grid width={300} height={200} rowCount={data.length} columnCount={2} rowHeight={40} columnWidth={150} cellRenderer={cellRenderer} />
     </div>
   );
 }
